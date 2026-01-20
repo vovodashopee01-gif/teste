6fba7a824ed234efde35f4d267bc5ecabe3a8fe5
vovodashopee01-gif <vovodashopee01@gmail.com>
2026-01-20 09:54:52 -0300

    alteração

--- teste.js
+++ teste.js
@@ -3,4 +3,12 @@ AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
 ola bom dia
 OI
+
 fsadfsdfaa
+
+
+
+let passagem = "10 reais"
+let pergunta = "quanto custa a passagem?"
+console.log(pergunta, passagem)
+
